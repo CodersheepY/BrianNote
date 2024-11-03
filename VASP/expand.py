ba6ed9d7a80710c822a099e396ece77c58fc7d3b
CodersheepY <longyang2000110@gmail.com>
2024-11-03 14:28:59 +0900

    updated

---
+++ VASP/expand.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys, os
+import ase.io.vasp
+
+file_read = sys.argv[1]
+x,y,z     = [int(i) for i in sys.argv[2:5]]
+try:
+    cell = ase.io.vasp.read_vasp("CONTCAR")
+    ase.io.vasp.write_vasp("POSCAR",cell*(x, y, z), direct=True,sort=True)
+except:
+    print(os.getcwd())
